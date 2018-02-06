6122431c573f80799f77d948bcfc78d7814aed02
m3hrad <mehrad.mohammadi@futurice.com>
2018-02-06 17:17:03 +0200

    sample member data added to Elixia

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 var fakeCurrentCommunityJson = {'id': 0, 'name': 'Elixia Kamppi', 'address': 'Fredrikinkatu 48, 00100 Helsinki',
-'members': [1,2,3], 'rate': 13, 'img_url':'https://s3.eu-central-1.amazonaws.com/relivee/elixia_kamppi.jpg'};
+    'rate': 13, 'img_url':'https://s3.eu-central-1.amazonaws.com/relivee/elixia_kamppi.jpg',
+    'members': [{'id': 0, 'name': 'Mehrad' , 'communityRate': 90, 'totalRate': 80, 'interactionsWithUser' : 1,
+        'imageUrl': 'https://s3.eu-central-1.amazonaws.com/relivee/profile/0.jpg'},
+        {'id': 1, 'name': 'Barack' , 'communityRate': 100, 'totalRate': 100, 'interactionsWithUser' : 0,
+        'imageUrl': 'https://s3.eu-central-1.amazonaws.com/relivee/profile/1.jpg'}
+        ]};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
