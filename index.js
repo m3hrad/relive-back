f2d8685f5bef5d86cd898a0b98eaa294a54731c1
m3hrad <mehrad.mohammadi@futurice.com>
2018-02-06 10:27:51 +0200

    fake current location api added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+var fakeCurrentCommunityJson = {'name': 'Elixia Kamppi', 'address': 'Fredrikinkatu 48, 00100 Helsinki',
+'members': [1,2,3], 'rate': 13};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/communities/current', (req, res) => res.json(fakeCurrentCommunityJson))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
